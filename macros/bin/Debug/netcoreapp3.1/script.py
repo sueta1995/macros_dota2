a474947d30fcfd61d4b4f9663802d10518ddbd36
tageerBOY <kitaev.tagir@gmail.com>
2020-06-20 18:10:49 +0300

    beta version 0.1.1

---
+++ macros/bin/Debug/netcoreapp3.1/script.py
@@ -0,0 +1,32 @@
+import keyboard
+import pydirectinput
+import os
+
+def print_pressed_keys(e):
+	try:
+		with open('config.properties', 'r') as file_handler:
+		    for line in file_handler:
+		    	if UPDATE_VALUE(line.split(' ')[1]) == e.name and e.event_type == 'down':
+		        	print(">> Start shortcut process...")
+		        	for i in range(2, len(line.split(' '))):
+		        		pydirectinput.press(UPDATE_VALUE(line.split(' ')[i]))
+		        		print(">> Pressed {0}".format(line.split(' ')[i]))
+	
+	except:
+		print(">> An error occured!")
+
+def UPDATE_VALUE(KEY):
+	KEY = KEY.lower()
+
+	if KEY == "spacebar":
+		KEY = "space"
+	elif KEY == "escape":
+		KEY = "esc"
+	elif len(KEY) == 2 and KEY[0] == "d":
+		KEY = KEY[1]
+
+	return KEY
+
+
+keyboard.hook(print_pressed_keys)
+keyboard.wait()
